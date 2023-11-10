7085090c8fe5ff03843c1d57f387ab4115714dd1
jabongg <jbpvns@gmail.com>
2023-11-10 16:41:45 +0530

    buttons working

--- src/App.jsx
+++ src/App.jsx
@@ -8,54 +8,39 @@ function App() {
   const [color, setColor] = useState("olive");
 
   return (
-    <>
-    <div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
 
-    <div className="w-full h-screen" style={{backgroundColor : color}}></div>
-    <div className="flex flex-wrap justify-center bottom-12 inset-x-0 px-2">test</div>
+   
+   <div>
+   <div className="w-full h-screen " style={{backgroundColor : color}}></div>
+
     <div className=" shadow-xl bg-white flex flex-wrap justify-center px-3 py-2 rounded-3xl">
-        <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        <button onClick={() => setColor("red")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
         style={{backgroundColor: "red"}}>
           red
         </button>
     </div>
 
     <div className=" shadow-xl bg-white flex flex-wrap justify-center px-3 py-2 rounded-3xl">
-        <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        <button onClick={() => setColor("green")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
         style={{backgroundColor: "green"}}>
           green
         </button>
     </div>
 
-    <div className=" shadow-xl bg-white flex flex-wrap justify-center px-3 py-2 rounded-3xl">
+    <div onClick={() => setColor("blue")} className=" shadow-xl bg-white flex flex-wrap justify-center px-3 py-2 rounded-3xl">
         <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
         style={{backgroundColor: "blue"}}>
           blue
         </button>
     </div>
+    <div onClick={() => setColor("olive")} className=" shadow-xl bg-white flex flex-wrap justify-center px-3 py-2 rounded-3xl">
+        <button className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+       style={{backgroundColor: "white"}}>
+          reset
+        </button>
+    </div>
+    </div>
 
-    </>
   )
 }
 
